e318d02356649ca741aced57f3756a4bb965e82a
velocichaptor <jake.m.chappelle@gmail.com>
2020-09-16 07:59:01 -0500

    finished lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+
+attr_accessor :roster
+
+     def initialize(name)
+     @name = name
+     @roster = {}
+     end
+
+     def add_student(student, grade)
+
+         if !!roster[grade]
+             roster[grade]<<student
+         else
+             roster[grade] = []
+             roster[grade] << student
+         end
+     end
+
+     def grade(student)
+         roster[student]
+     end
+
+     def sort
+         sorted_by_grades = {}
+         roster.each do |k,v|
+             sorted_by_grades[k] = v.sort
+         end
+         p sorted_by_grades
+     end
+
+ end
